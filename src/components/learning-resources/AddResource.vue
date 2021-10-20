0777fc9faa29806f7202907d9983bbb022667ed5
xxx <moldovan.alexandra28@gmail.com>
2021-10-20 17:51:25 +0300

    adding a form

---
+++ src/components/learning-resources/AddResource.vue
@@ -0,0 +1,18 @@
+<template>
+<h2>Add Resource</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedTab: 'stored-resources'
+        };
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
