612160cc6ffebb655c21b623cd00a7beac6f0286
xxx <moldovan.alexandra28@gmail.com>
2021-10-20 15:50:18 +0300

    adding BaseButton

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import BaseCard from './components/UI/BaseCard.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButtton from './components/UI/BaseButton.vue'
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
-
+app.component('base-button', BaseButtton);
 app.mount('#app');
